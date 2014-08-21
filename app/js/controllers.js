650a9ac97b164b1bb753c55ef5c728448d864c6d
Yadiel Arroyo <yadielar@gmail.com>
2014-08-21 18:20:52 -0400

    Created a ParsedContents factory for easier reuse in controllers.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,9 @@
 /* Controllers */
 
 angular.module('talcApp.controllers', [])
-	.controller('EditorController', ['$scope', function($scope) {
+	.controller('EditorController', ['$scope', 'ParsedContents', function($scope, ParsedContents) {
 		$scope.editorContents = localStorage.getItem('talcContent') || "";
-		$scope.parsedContents = [];
-		$scope.results = [];
+		$scope.parsedContents = {};
 		this.save = function(){
 			localStorage.setItem('talcContent', $scope.editorContents);
 			//console.log("Saved:\n"+$scope.editorContents);
@@ -14,56 +13,11 @@ angular.module('talcApp.controllers', [])
 		this.clear = function(){
 			localStorage.clear();
 			$scope.editorContents = "";
-			$scope.results = [];
-		};
-		this.addResults = function(){
-			var parsed = $scope.parsedContents;
-			$scope.results = [];
-			for (var i=0, x=parsed.length; i < x; i++) {
-				$scope.results[i] = {};
-				$scope.results[i].original = parsed[i].original;
-				try {
-					$scope.results[i].val = eval(parsed[i].parsed);
-				} catch(err) {
-					console.log(parsed[i].parsed+" is not a valid expression.");
-				}
-				$scope.results[i].val = $scope.results[i].val || "";
-			}
-			console.log($scope.results);
+			$scope.parsedContents = {};
 		};
 		this.parse = function(){
-			var contents = $scope.editorContents,
-				currentChar,
-				lineCount = 0,
-				operators = ["+","-","*","/","(",")"],
-				clean = [];
-			function isNumber(n) {
-				return !isNaN(parseFloat(n)) && isFinite(n);
-			};
-			for (var i=0, x=contents.length; i < x; i++) {
-				var prevChar = contents.charAt(i-1),
-					nextChar = contents.charAt(i+1);
-				currentChar = contents.charAt(i);
-				clean[lineCount] = clean[lineCount] || {};
-				clean[lineCount].original = clean[lineCount].original || "";
-				clean[lineCount].original += currentChar;
-				clean[lineCount].parsed = clean[lineCount].parsed || "";
-				if (operators.indexOf(currentChar) > -1 || isNumber(currentChar)) {
-					clean[lineCount].parsed += currentChar;
-				} else if (currentChar === ".") {
-					if ( isNumber(prevChar) && isNumber(nextChar) ) {
-						clean[lineCount].parsed += currentChar;
-					}
-				} else if (currentChar === "\n") {
-					/*if (clean[lineCount] === undefined) {
-						clean[lineCount] = 0;
-					}*/
-					lineCount++;
-				}
-			}
-			$scope.parsedContents = clean;
+			$scope.parsedContents = new ParsedContents($scope.editorContents);
 			console.log($scope.parsedContents);
-			this.addResults();
 			this.save();
 		};
 	}])
--- app/js/services.js
+++ app/js/services.js
@@ -2,8 +2,59 @@
 
 /* Services */
 
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
 angular.module('talcApp.services', []).
-  value('version', '0.1');
+	value('isNumber', function(n) {
+		return !isNaN(parseFloat(n)) && isFinite(n);
+	}).
+	factory('ParsedContents', ['isNumber', function(isNumber) {
+		function ParsedContents(contents) {
+			this.lines = this.parse(contents);
+			this.results = this.getResults(this.lines);
+			this.sum = 0;
+		}
+		ParsedContents.prototype = {
+			parse: function(contents) {
+				var currentChar,
+					lineCount = 0,
+					operators = ["+","-","*","/","(",")"],
+					clean = [];
+				for (var i=0, x=contents.length; i < x; i++) {
+					var prevChar = contents.charAt(i-1),
+						nextChar = contents.charAt(i+1);
+					currentChar = contents.charAt(i);
+					clean[lineCount] = clean[lineCount] || {};
+					clean[lineCount].original = clean[lineCount].original || "";
+					clean[lineCount].original += currentChar;
+					clean[lineCount].parsed = clean[lineCount].parsed || "";
+					if (operators.indexOf(currentChar) > -1 || isNumber(currentChar)) {
+						clean[lineCount].parsed += currentChar;
+					} else if (currentChar === ".") {
+						if ( isNumber(prevChar) && isNumber(nextChar) ) {
+							clean[lineCount].parsed += currentChar;
+						}
+					} else if (currentChar === "\n") {
+						/*if (clean[lineCount] === undefined) {
+							clean[lineCount] = 0;
+						}*/
+						lineCount++;
+					}
+				}
+				return clean;
+			},
+			getResults: function(lines) {
+				var results = [];
+				for (var i=0, x=lines.length; i < x; i++) {
+					results[i] = {};
+					results[i].original = lines[i].original;
+					try {
+						results[i].val = eval(lines[i].parsed);
+					} catch(err) {
+						console.log(lines[i].parsed+" is not a valid expression.");
+					}
+					results[i].val = results[i].val || "";
+				}
+				return results;
+			}
+		};
+		return ParsedContents;
+	}]);
